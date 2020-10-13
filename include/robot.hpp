33989b2c293bde704b536851223ddd97c052b34c
Govind <govindajithkumar@gmail.com>
2020-10-13 15:12:42 -0400

    [Task1.6] Created Robot stub class
    [robot.hpp] Created robot class stubs

---
+++ include/robot.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+class Robot{
+private:
+	double wheel_base;
+	double wheel_track;
+	double wheel_radius;
+	double robot_heading;
+	double robot_velocity;
+	double inner_wheel_velocity;
+	double outer_wheel_velocity;
+public:
+	// Robot(){};
+	void drive(double,double){};
+}
+
+
