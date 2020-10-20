1c3ed30786573ccadee8fba6a9a483932c805f53
Govind <govindajithkumar@gmail.com>
2020-10-20 14:37:00 -0400

    [Task1.13] Modified Robot stubs and doxygen commenting
    [robot.hpp] added new private variables and commented
    
    [robot.cpp] Added Doxygen Documentation

--- app/robot.cpp
+++ app/robot.cpp
@@ -1,4 +1,10 @@
 #include <robot.hpp>
 
+/**
+ * @brief      The function to drive the robot and move it forwards
+ *
+ * @param[in]  throttle        The throttle
+ * @param[in]  steering_angle  The steering angle
+ */
 void Robot::drive(double throttle,double steering_angle){
 }
