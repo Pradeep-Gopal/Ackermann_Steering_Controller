5bcd172fc57ab9426dc19f434eac3b94e44de17f
Govind <govindajithkumar@gmail.com>
2020-10-20 14:45:46 -0400

    [Task1.16] Modified Analysis interface stubs and doxygen commenting
    [analysis.hpp] Did doxygen commenting
    
    [analysis.cpp] Added Doxygen Documentation

--- app/analysis.cpp
+++ app/analysis.cpp
@@ -1,5 +1,8 @@
 #include <analysis.hpp>
 
+/**
+ * @brief      shows the convergence from set points to the desired points 
+ */
 void Analysis::converge(){
 	
 }
--- include/analysis.hpp
+++ include/analysis.hpp
@@ -1,8 +1,13 @@
 #pragma once
-
+/**
+ * @brief      This class instantiates all the objects
+ */
 class Analysis{
 public:
 	// Analysis(){};
+	/**
+ * @brief      shows the convergence from set points to the desired points 
+ */
 	void converge();
 };
 
